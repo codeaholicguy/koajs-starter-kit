f987987d0d13401c1f92f6436d2f3d5b5411ace9
Hoang Nhat Nguyen <hoangnn93@gmail.com>
2017-10-31 23:07:51 +0700

    Add test

--- src/app.js
+++ src/app.js
@@ -45,6 +45,10 @@ export default class App {
     })
   }
 
+  server() {
+    return this._httpServer
+  }
+
   //
   // Private methods
   //
--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ const app = new App({
 })
 
 app.listen()
+
+export default app.server()
